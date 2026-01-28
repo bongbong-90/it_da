da0d28ee9e285af489206748197582c3615b85b7
bongbong-90 <ftsyuw@naver.com>
2026-01-28 15:54:27 +0900

    주최자 모집 완료시 타 유저 모임 참여 비활성화

--- it-da-frontend/dev-dist/sw.js
+++ it-da-frontend/dev-dist/sw.js
@@ -78,8 +78,11 @@ define(['./workbox-237f2c1f'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "suppress-warnings.js",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  }, {
     "url": "/index.html",
-    "revision": "0.kv1a8l0va88"
+    "revision": "0.cje8fsi3l0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
